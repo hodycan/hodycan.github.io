b345bd4d69f7c431f6759b7710b3ec1b540eab84
hodycan <hodycan@gmail.com>
2015-05-04 20:23:47 -0700

    initial

---
+++ submission.js
@@ -0,0 +1,39 @@
+
+
+function emptySubmissionFields() {
+    $('.submit-body').val('');
+    $('.submit-title').val('');
+};
+
+function main() {
+
+    emptySubmissionFields();
+
+    // Add the post to page and clear the text fields.
+    $('.submit-button').click(function() {
+        var title = $('.submit-title').val()
+            body = $('.submit-body').val(),
+            postList = $('.post-list'),
+            newPost = $('<li>').addClass('post');
+
+        newPost.append('<a href="#"><h2>'+ title +'</h2></a>');
+        newPost.append("<small><p .class='post-date'>MM/DD/YYYY </p>Posted by: <p .class='post-submitter'>First Last</p></small>")
+        newPost.append('<p>' + body + '</p>');
+        postList.prepend(newPost);
+
+        emptySubmissionFields();
+
+        $('.submit-button').addClass('disabled');
+    });
+
+    // Makes sure the submit button is disabled when text fields are empty.
+    $('.submit-body, .submit-title').keyup(function() {
+        var title = $('.submit-title').val();
+        var body = $('.submit-body').val();
+        if (title.length > 0 && body.length > 0) {
+            $('.submit-button').removeClass('disabled');
+        }
+    })
+};
+
+$(document).ready(main);
