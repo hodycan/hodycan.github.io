41e36f10391cd46dd8c29935237f47fc9878fd64
hodycan <hodycan@gmail.com>
2015-05-20 23:59:45 -0700

    Add tab functionality for post submission

--- submission.js
+++ submission.js
@@ -1,41 +1,44 @@
-function emptySubmissionFields() {
-    $('.submit-body').val('');
-    $('.submit-title').val('');
-};
+function emptySubmissionFields(group) {
+    for (i=0; i < group.length; i++) {
+        $(group[i]).val('');
+    }
+}
 
-function submitPost() {
-    var title = $('.submit-title').val(),
-        body = $('.submit-body').val(),
-        postList = $('.post-list'),
-        newPost = $('<li>').addClass('post');
+function submitAnnouncement() {
+    var title = $('.announcement-title').val(),
+        body = $('.announcement-body').val(),
+        postList = $('.newsfeed'),
+        newPost = $('<div>').addClass('post');
 
     newPost.append('<a href="#"><h2>'+ title +'</h2></a>');
     newPost.append("<small><p .class='post-date'>MM/DD/YYYY </p>Posted by: <p .class='post-submitter'>First Last</p></small>");
     newPost.append('<p>' + body + '</p>');
     postList.prepend(newPost);
 
-    emptySubmissionFields();
+    emptySubmissionFields(announcementGroup);
 
-    $('.submit-button').addClass('disabled');
+    $('.post-announcement').addClass('disabled');
 }
 
 function submitRefresher() {
-    var title = $('.submit-title').val();
-    var body = $('.submit-body').val();
+    var title = $('.announcement-title').val();
+    var body = $('.announcement-body').val();
     if (title.length > 0 && body.length > 0) {
-        $('.submit-button').removeClass('disabled');    
+        $('.post-announcement').removeClass('disabled');    
     }
 }
 
+
+
 function main() {
 
-    emptySubmissionFields();
+    announcementGroup = ['.announcement-body', '.announcement-title'];
 
-    // Add the post to page and clear the text fields.
-    $('.submit-button').click(submitPost);
-
+    emptySubmissionFields(announcementGroup);
     // Makes sure the submit button is disabled when text fields are empty.
-    $('.submit-body, .submit-title').keyup(submitRefresher);
+    $('.announcement-body, .announcement-title').keyup(submitRefresher);
+    // Add the post to page and clear the text fields.
+    $('.post-announcement').click(submitAnnouncement);
 };
 
 $(document).ready(main);
