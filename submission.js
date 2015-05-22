efd9edc320474c8183c2c41c900fa9a7dbe1e48d
hodycan <hodycan@gmail.com>
2015-05-21 21:09:57 -0700

    Progress on survey functionality

--- submission.js
+++ submission.js
@@ -1,6 +1,12 @@
-function emptySubmissionFields(group) {
-    for (i=0; i < group.length; i++) {
-        $(group[i]).val('');
+/*
+TODO:
+- add submit survey functionality
+... or maybe not and use google surveys?
+*/
+
+function emptySubmissionFields(submitGroup) {
+    for (i=0; i < submitGroup.length; i++) {
+        $(submitGroup[i]).val('');
     }
 }
 
@@ -9,36 +15,39 @@ function submitAnnouncement() {
         body = $('.announcement-body').val(),
         postList = $('.newsfeed'),
         newPost = $('<div>').addClass('post');
-
     newPost.append('<a href="#"><h2>'+ title +'</h2></a>');
     newPost.append("<small><p .class='post-date'>MM/DD/YYYY </p>Posted by: <p .class='post-submitter'>First Last</p></small>");
     newPost.append('<p>' + body + '</p>');
     postList.prepend(newPost);
-
-    emptySubmissionFields(announcementGroup);
-
-    $('.post-announcement').addClass('disabled');
+    emptySubmissionFields(announcementFields);
+    $('.post-announcement-btn').addClass('disabled');
 }
 
-function submitRefresher() {
-    var title = $('.announcement-title').val();
-    var body = $('.announcement-body').val();
-    if (title.length > 0 && body.length > 0) {
-        $('.post-announcement').removeClass('disabled');    
+function bindCheckAllowPosting(refreshGroup, button) {
+    function checkAllowPosting() {
+        for (i=0; i < refreshGroup.length; i++) {
+            if ($(refreshGroup[i]).val().length < 1) {
+                return;
+            }
+        }
+    $(button).removeClass('disabled');
     }
+    for (i=0; i < refreshGroup.length; i++) {
+        $(refreshGroup[i]).keyup(checkAllowPosting)
+    }
+
 }
-
-
 
 function main() {
+    announcementFields = ['.announcement-body', '.announcement-title'];
+    announcementRefreshFields = ['.announcement-body', '.announcement-title'];
+    surveyFields = ['.survey-title', '.survey-choice']
+    surveyRefreshFields = ['.survey-title', '.survey-choice:first-child']
+    emptySubmissionFields(announcementFields);
 
-    announcementGroup = ['.announcement-body', '.announcement-title'];
-
-    emptySubmissionFields(announcementGroup);
-    // Makes sure the submit button is disabled when text fields are empty.
-    $('.announcement-body, .announcement-title').keyup(submitRefresher);
-    // Add the post to page and clear the text fields.
-    $('.post-announcement').click(submitAnnouncement);
+    bindCheckAllowPosting(announcementRefreshFields, '.post-announcement-btn')
+    $('.post-announcement-btn').click(submitAnnouncement);
+    bindCheckAllowPosting(surveyRefreshFields, '.post-survey-btn');
 };
 
 $(document).ready(main);
