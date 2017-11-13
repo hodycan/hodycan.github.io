04b95c67c960952ee6c55737a16032e5c47bf078
Cody Han <hodycan@gmail.com>
2017-11-13 07:40:10 -0800

    updated website

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".centered h1").hover(function(){
+        $(this).css("color", "yellow");
+        }, function(){
+        $(this).css("color", "#3DBDB2");
+    });
+});
