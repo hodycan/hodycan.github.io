19338ce457e2b0d82e92be1bc8d206c3359630cc
Cody Han <hodycan@gmail.com>
2017-11-13 18:15:38 -0800

    updated email link

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-    $(".centered h1").hover(function(){
-        $(this).css("color", "yellow");
-        }, function(){
-        $(this).css("color", "#3DBDB2");
-    });
-});
+
+    $(".centered h1").hover(
+    	function(){
+        	$(this).css("color", "yellow");
+        }, 
+        function(){
+        	$(this).css("color", "#3DBDB2");
+    }),
+
+    $("#email").hover(
+    	function(){
+        	$(this).css("color", "yellow");
+        }, 
+        function(){
+        	$(this).css("color", "#3DBDB2");
+    	});
+	}
+);
